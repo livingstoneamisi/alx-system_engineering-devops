c2132cd31008a332fad7b7b6dca78164e0a50e00
livingstoneamisi <livingstoneamisi00@gmail.com>
2024-05-31 15:13:45 +0300

    1. Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,13 @@
-#!/usr/bin/puppet
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+# install flask using Puppet
+
+exec { 'install_flask':
+    command => '/usr/bin/pip3 install flask==2.1.0',
+    path    => ['/usr/bin', '/bin'],
+    unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
+}
+
+exec { 'install_werkzeug':
+    command => '/usr/bin/pip3 install werkzeug==2.1.1',
+    path    => ['/usr/bin', '/bin'],
+    unless  => '/usr/bin/pip3 show werkzeug | grep -q "Version: 2.1.1"',
 }
