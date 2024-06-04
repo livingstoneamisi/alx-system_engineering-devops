a6c53e8c40fdd4fcc0bed06d4e18c86265503946
livingstoneamisi <livingstoneamisi00@gmail.com>
2024-06-05 00:51:57 +0300

    0. How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,34 +1,22 @@
 #!/usr/bin/python3
-"""
-Function queries Reddit API and returns the number of subscribers
-"""
+"""Return the total number of subscribers on a given subreddit."""
+
 import requests
 
+
 def number_of_subscribers(subreddit):
-    """Returns the total number of subscribers
-    for a given subreddit.
     """
-    # Set the Default URL strings
-    base_url = 'https://www.reddit.com'
-    api_uri = '{base}/r/{subreddit}/about.json'.format(base=base_url,
-                                                       subreddit=subreddit)
+    Queries the Reddit API and returns
+    the number of subscribers for a given subreddit.
+    If an invalid subreddit is given, the function should return 0.
+    """
 
-    # Set a custom User-Agent
-    user_agent = {'User-Agent': 'Custom User Agent'}
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "linux:0x16.api.advanced"}
+    response = requests.get(url=url, headers=headers, allow_redirects=False)
 
-    # Get the Response of the Reddit API
-    res = requests.get(api_uri, headers=user_agent,
-                       allow_redirects=False)
-
-    # Checks if the subreddit is invalid
-    if res.status_code == 404:
+    if response.status_code == 404:
         return 0
 
-    # Handle redirects if necessary
-    if res.status_code == 302:
-        # Redirects are not followed, hence an invalid subreddit
-        return 0
-
-    # Returns the total subscribers of the subreddit
-    return res.json().get('data', {}).get('subscribers', 0)
-
+    results = response.json().get("data")
+    return results.get("subscribers")
