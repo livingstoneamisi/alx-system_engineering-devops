71142f2e7b88efd7faa25e45a9384c3b0fd8e72f
livingstoneamisi <livingstoneamisi00@gmail.com>
2024-06-05 00:50:16 +0300

    0. How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,7 +4,6 @@ Function queries Reddit API and returns the number of subscribers
 """
 import requests
 
-
 def number_of_subscribers(subreddit):
     """Returns the total number of subscribers
     for a given subreddit.
@@ -14,16 +13,22 @@ def number_of_subscribers(subreddit):
     api_uri = '{base}/r/{subreddit}/about.json'.format(base=base_url,
                                                        subreddit=subreddit)
 
-    # Set an User-Agent
-    user_agent = {'User-Agent': 'Python/requests'}
+    # Set a custom User-Agent
+    user_agent = {'User-Agent': 'Custom User Agent'}
 
     # Get the Response of the Reddit API
     res = requests.get(api_uri, headers=user_agent,
                        allow_redirects=False)
 
     # Checks if the subreddit is invalid
-    if res.status_code in [302, 404]:
+    if res.status_code == 404:
+        return 0
+
+    # Handle redirects if necessary
+    if res.status_code == 302:
+        # Redirects are not followed, hence an invalid subreddit
         return 0
 
     # Returns the total subscribers of the subreddit
-    return res.json().get('data').get('subscribers')
+    return res.json().get('data', {}).get('subscribers', 0)
+
